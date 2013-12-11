3ca51b9e529c1a664d3a8a35ff07de8ad99cf396
Scott <sscotth@gmail.com>
2013-12-11 13:30:31 -0600

    working add and get item

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,11 +1,30 @@
+require 'linked_list_item'
+
 class LinkedList
 
-  def add_item(payload)
+  def initialize
+    @first_item = nil
+  end
 
+  def add_item(payload)
+    new_item = LinkedListItem.new(payload)
+    if @first_item.nil?
+      @first_item = new_item
+    else
+      last_item = @first_item
+      until last_item.nil? || last_item.last?
+        last_item = last_item.next_list_item
+      end
+      last_item.next_list_item = new_item
+    end
   end
 
   def get(item_index)
-    'foo'
+    item = @first_item
+    item_index.times do
+      item = item.next_list_item
+    end
+    item.payload
   end
 
 end
