ccb7c49772a26546ba5c2778a17cbfc90a1939e1
Scott <sscotth@gmail.com>
2013-12-11 12:05:50 -0600

    next_list_item method

--- lib/linked_list_item.rb
+++ lib/linked_list_item.rb
@@ -1,11 +1,17 @@
 class LinkedListItem
 
+  attr_reader :next_list_item
+
   def initialize(payload)
 
   end
 
   def payload
     'foo'
+  end
+
+  def next_list_item=(linked_list_item)
+    @next_list_item = linked_list_item
   end
 
 end
